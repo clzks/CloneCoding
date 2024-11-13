27b9903d285aeaf1f5ccb40bd1ca1e95726072b3
clzks <clzks@naver.com>
2024-11-13 15:32:05 +0900

    임시 footer 완료

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,9 +5,18 @@ const Footer = () => {
 
     return (
         <div className="footer">
-            <div className="footer-item"></div>
-            <div className="footer-item"></div>
-            <div className="footer-item"></div>
+            <div className="footer-container">
+                <div className="footer-item">이용 약관</div>
+                <div className="footer-item">Operated by Emanruoy Inc.</div>
+                <div className="footer-item">Current timezone Asia/Seoul</div>
+            </div>
+            <div className="footer-container">
+                <div className="footer-item">Location Seoul, South Korea</div>
+                <div className="footer-item">Copyright © 2024 YourName Inc. All rights reserved</div>
+            </div>
+            <div className="footer-container">
+                <div className="footer-item">Security Notice This site is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply</div>
+            </div>
         </div>
     )
 }
